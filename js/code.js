6149bcb8ef99b848422ca8255815d48afa536cd7
Jacob Pacheco <102394465+IcyPistachio@users.noreply.github.com>
2024-05-23 00:38:03 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -232,7 +232,8 @@ function searchContact()
 					// This is responsible for how the List of contacts look *****IMPORTANT****
 					contactList += jsonObject.results[i].FirstName + " | " + jsonObject.results[i].LastName + " | " + jsonObject.results[i].Phone + " | " + jsonObject.results[i].Email;
 					
-					
+					contactList += " " + "<button type='button' onclick='deleteContact();'> Delete</button>";
+					contactList += " " + "<button type='button' onclick='editContact();'> Edit</button>";
 
 					if( i < jsonObject.results.length - 1 )
 					{
@@ -252,52 +253,11 @@ function searchContact()
 	}
 }
 
-function deleteRow(id) {
+function deleteContact() {
 	
 }
 
-function searchColor()
-{
-	let srch = document.getElementById("searchText").value;
-	document.getElementById("colorSearchResult").innerHTML = "";
-	
-	let colorList = "";
-
-	let tmp = {search:srch,userId:userId};
-	let jsonPayload = JSON.stringify( tmp );
-
-	let url = urlBase + '/SearchColors.' + extension;
-	
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-				let jsonObject = JSON.parse( xhr.responseText );
-				
-				for( let i=0; i<jsonObject.results.length; i++ )
-				{
-					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						colorList += "<br />\r\n";
-					}
-				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("colorSearchResult").innerHTML = err.message;
-	}
+function editContact() {
 	
 }
 
